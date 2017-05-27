9319408cd8356b0df6f09bd770e1d515ee4dbbc8
Adrian Dela Piedra <adrian.onrails@gmail.com>
2017-05-27 19:26:04 +0800

    TG-64 Changed default font size

--- src/common/models/reader-settings.ts
+++ src/common/models/reader-settings.ts
@@ -1,7 +1,7 @@
 export class ReaderSettings {
     public _id: string = "unique";
     public _rev: number;
-    public fontSize: number = 18;
+    public fontSize: number = 15;
     public brightness: number = 1;
     public invertColors: boolean = false;
     public horizontalScrolling: boolean = false;
