360f2f18f0f3b474294538448a78b8d5df1cba9a
Adrian Dela Piedra <adrian.onrails@gmail.com>
2017-07-18 14:17:08 +0800

    Update travis configuration to automatically sign APK

--- travis/package-android.sh
+++ travis/package-android.sh
@@ -9,5 +9,8 @@ then
 fi
 
 mkdir -p output
-cp platforms/android/build/outputs/apk/android-debug.apk output/baruch-debug.apk
+cp platforms/android/build/outputs/apk/android-release-unsigned.apk output/baruch-release-unsigned.apk
 
+#sign the apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks -storepass $1 baruch-release-unsigned.apk adrianonrails
+"${ANDROID_HOME}/build-tools/26.0.0/zipalign" -v 4 output/baruch-release-unsigned.apk output/Baruch.apk
