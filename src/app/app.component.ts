e03d42f32cace8f92806b73086e8fd612b6c91d4
Adrian Dela Piedra <adrian.onrails@gmail.com>
2017-07-19 20:03:06 +0800

    #10 degrade, back button doesn't work now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,7 +62,7 @@ export class MyApp {
       this.loadingController.init();
 
       // Register back button
-      this.platform.registerBackButtonAction(this.backButtonHandler, 0);
+      this.registerBackButtonHandler();
     });
   }
 
@@ -105,41 +105,43 @@ export class MyApp {
     });
   }
 
-  private backButtonHandler() {
+  private registerBackButtonHandler() {
     // Taken from https://stackoverflow.com/a/44365055 and https://github.com/ionic-team/ionic/issues/6982#issuecomment-295896544
     // Back button handle
     // Registration of push in Android and Windows Phone
     var lastTimeBackPress = 0;
     var timePeriodToExit = 2000;
 
-    let activePortal = this.ionicApp._loadingPortal.getActive() ||
-      this.ionicApp._modalPortal.getActive() ||
-      this.ionicApp._overlayPortal.getActive();
+    this.platform.registerBackButtonAction(() => {
+      let activePortal = this.ionicApp._loadingPortal.getActive() ||
+        this.ionicApp._modalPortal.getActive() ||
+        this.ionicApp._overlayPortal.getActive();
 
-    //activePortal is the active overlay like a modal,toast,etc
-    if (activePortal) {
-      activePortal.dismiss();
-      return
-    }
-
-    let view = this.nav.getActive(); // As none of the above have occurred, its either a page pushed from menu or tab
-
-    if (this.nav.canGoBack() || view && view.isOverlay) {
-      this.nav.pop(); //pop if page can go back or if its an overlay over a menu page
-    }
-    else {
-      // Double check to exit app
-      if (new Date().getTime() - lastTimeBackPress < timePeriodToExit) {
-        this.platform.exitApp(); // Exit from app
-      } else {
-        let toast = this.toastCtrl.create({
-          message: "Press back again to exit the application.",
-          duration: timePeriodToExit,
-          position: "bottom"
-        });
-        toast.present();
-        lastTimeBackPress = new Date().getTime();
+      //activePortal is the active overlay like a modal,toast,etc
+      if (activePortal) {
+        activePortal.dismiss();
+        return
       }
-    }
+
+      let view = this.nav.getActive(); // As none of the above have occurred, its either a page pushed from menu or tab
+
+      if (this.nav.canGoBack() || view && view.isOverlay) {
+        this.nav.pop(); //pop if page can go back or if its an overlay over a menu page
+      }
+      else {
+        // Double check to exit app
+        if (new Date().getTime() - lastTimeBackPress < timePeriodToExit) {
+          this.platform.exitApp(); // Exit from app
+        } else {
+          let toast = this.toastCtrl.create({
+            message: "Press back again to exit the application.",
+            duration: timePeriodToExit,
+            position: "bottom"
+          });
+          toast.present();
+          lastTimeBackPress = new Date().getTime();
+        }
+      }
+    }, 0);
   }
 }
